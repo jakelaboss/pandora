2ff7e59534d396f09752e8d97650e5f4c9a52f85
Jake LaBossier <jakelaboss@gmail.com>
2024-02-29 17:12:32 -0500

    updates license and defines project.clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject jakelaboss/pandora "0.0.1"
+  :description "Pandoric Macro System"
+  :authors [{:name "Jake LaBossier"} {:name "Doug Hoyte"}]
+  :license {:name "BSD 2-Clause License"
+            :url "http://opensource.org/licenses/BSD-2-Clause"}
+  :url "localhost"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/tools.macro "0.2.0"]]
+  :main pandora.core)
