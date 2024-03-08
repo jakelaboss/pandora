5a91730109947f263c907f9a3181764b92677afb
Jake LaBossier <jakelaboss@gmail.com>
2024-03-08 08:05:51 -0500

    converts atoms in pandoric scope to unsynchronized-mutable objects

--- src/pandora/core.clj
+++ src/pandora/core.clj
@@ -1,6 +1,30 @@
 (ns pandora.core
   (:require
-   [clojure.tools.macro :as macros]))
+   [clojure.tools.macro :as macros]
+    [clojure.pprint :as pprint]))
+
+(definterface IOObject
+  (setVal [new-val])
+  (getVal [])
+  (oswap [f])
+  (oswap [f x])
+  (oswap [f x y])
+  (oswap [f x y z]))
+
+(deftype meta-object
+         [^:unsynchronized-mutable val]
+  IOObject
+  (setVal [o new-val] (set! val new-val))
+  (getVal [o] val)
+  (oswap [o f] (set! val (f val)))
+  (oswap [o f x] (set! val (f val x)))
+  (oswap [o f x y] (set! val (f val x y)))
+  (oswap [o f x y z] (set! val (f val x y z)))
+  clojure.lang.IDeref
+  (deref [o] val))
+
+(let [y# (meta-object. 1)]
+  (.setVal y# 4))
 
 ;; --------------------------------------------------------------------------------
 ;; General Utility
@@ -10,6 +34,9 @@
   "Construct a symbol from a list of strings, symbols, or keywords."
   [& s]
   (symbol (clojure.core/apply str s)))
+
+(defn prt [x]
+  (pprint/pprint x) x)
 
 (defn find-by
   "Find the first item in a sequence that satisfies a predicate."
@@ -55,24 +82,18 @@
       (if (even? @pos)
         (do (swap! pos inc) (symb x "#"))
         (do (swap! pos inc)
-            `(atom  ~x))))))
+            `(meta-object.  ~x))))))
+
 
 (defn macrolet-binding-transform
   "Transforms macrolet bindings"
   [letargs]
   (let [pos (atom 0) ll (atom nil)]
-    (vec (concat (for [x letargs]
-                   (if (even? @pos)
-                     (do (swap! pos inc) (reset! ll x) x)
-                     (do (swap! pos inc)
-                         `(deref ~(symb @ll "#")))))
-                 (for [x letargs]
-                   (if (even? @pos)
-                     (do (swap! pos inc)
-                         (reset! ll x)
-                         (symb "x" x))
-                     (do (swap! pos inc)
-                         (symb ll "#"))))))))
+    (vec (for [x letargs]
+           (if (even? @pos)
+             (do (swap! pos inc) (reset! ll x) x)
+             (do (swap! pos inc)
+                 `(deref ~(symb @ll "#"))))))))
 
 (defn pget
   "Constructor for pget accessor"
@@ -99,7 +120,7 @@
   `(case (symb "%" ~'sym)
      ~@(mapcat (fn [x]
                `[~(symb "%" (first x))
-                 (reset! ~(symb (first x) "#") ~'val)])
+                 (.setVal ~(symb (first x) "#") ~'val)])
              letargs)
      :else "none found"))
 
@@ -109,7 +130,7 @@
   `(case (symb "%" ~'sym)
      ~@(mapcat (fn [x]
                `[~(symb "%" (first x))
-                 (swap! ~(symb (first x) "#") ~'fn)])
+                 (.oswap ~(symb (first x) "#") ~'fn)])
              letargs)
      :else "none found"))
 
@@ -117,7 +138,7 @@
   "Provides set macro atoms bound within the scope."
   [& body]
   `(macros/macrolet [(~'set [~'sym ~'val]
-                           `(reset! ~(symb ~'sym "#") ~~'val))]
+                           `(.setVal ~(symb ~'sym "#") ~~'val))]
                     ~@body))
 
 ;; --------------------------------------------------------------------------------
@@ -160,13 +181,14 @@
   --------------------------------------------------------------------------------
 "
   [letargs & body]
-  (let [letargs# (let-binding-transform (concat '(this (atom nil)) letargs))
+  (let [letargs# (let-binding-transform (concat '(this (meta-object. nil)) letargs))
         letlist# (group letargs 2)]
     `(let [~@letargs#]
        (macros/symbol-macrolet
         ~(macrolet-binding-transform letargs)
         ~@(butlast body)
         ~(atom-scope
+          `(.setVal ~'this# ~@body)
           `(d-fn
             (:pget [~'sym]
                    ~(pget letlist#))
@@ -179,7 +201,8 @@
             (:pget-vars []
                         (map #'first '~letlist#))
             (true [& ~'args]
-                  (apply ~(last body) ~'args))))))))
+                  ~'@this#
+                  (apply ~'@this# ~'args))))))))
 
 (defn get-p
   "Returns the value of a lexical binding from a pandoric function."
@@ -211,18 +234,31 @@
         syms))
     ~@body))
 
+(defn resolver [box]
+  (cond (fn? box) (var-get box)
+        (var? box) (var-get box)
+        (symbol? box) (or (resolve box) box)
+        :else (throw (ex-info "Invalid resolver" {:box box}))))
+
 (defmacro with-all-p
   "Provides access to all the lexical bindings within the scope.
   Allows a lexical scope to be passed around to different contexts, and operate on the lexical bindings."
   [box & body]
   `(macros/macrolet [(~'set [~'sym ~'val]
-                            `(set-p ~'~box '~~'sym ~~'val))]
+                            `(set-p ~'~box '~~'sym ~~'val))
+                     (~'swap [~'sym ~'fn]
+                             `(swap-p ~'~box '~~'sym ~~'fn))
+                     (~'get [~'sym]
+                            `(get-p ~'~box '~~'sym))
+                     (~'get-all []
+                                `(get-all-p ~'~box))]
                     (macros/symbol-macrolet
                      (~@(mapcat
                          (fn [x]
                            `(~x (get-p ~box '~x)))
-                         ((or (resolve box) box) :pget-vars)))
+                         ((resolver box) :pget-vars)))
                      ~@body)))
+
 
 (defmacro rescope-p
   "Resets the default function call of a pandoric function to a new function, with the same lexical bindings."
@@ -231,6 +267,13 @@
      (~(or (resolve pfn) pfn) :rescope
                        (fn ~fn-args
                          ~@body))))
+
+(let [this (meta-object. nil)]
+  (.setVal this 401)
+  (.getVal this))
+
+(eval `(let [~@(let-binding-transform (concat '(this nil) '(y 1 i 2)))]
+         ~'y#))
 
 (defmacro pfn
   "
@@ -255,8 +298,8 @@
        (macros/symbol-macrolet
         ~(macrolet-binding-transform (concat '(this nil) pan-args))
         ~(atom-scope
-          `(reset! ~'this#
-                   (fn ~fn-args ~@body))
+          `(.setVal ~'this#
+                    (fn ~fn-args ~@body))
           `(d-fn
             (:pget [~'sym]
                    ~(pget letlist#))
@@ -269,9 +312,10 @@
             (:pget-vars []
                         (map #'first '~letlist#))
             (:rescope [~'new]
-                      (reset! ~'this# ~'new))
+                      (.setVal ~'this# ~'new))
             (true [& ~'args]
                   (apply ~'@this# ~'args))))))))
+
 
 (defmacro defp
   "
@@ -291,16 +335,22 @@
   Allows for tunneling bindings into an eval expression.
   "
   [box expr]
-  `(let [p-eval-tunnel# '~box]
-     (eval `(~'with-all-p ~p-eval-tunnel#
+  `(binding [p-eval-tunnel '~box]
+     (eval `(~'with-all-p ~p-eval-tunnel
                           ~~expr))))
 
-(defmacro p-eval*
-  "A simplified version of p-eval that does not require the use of the with-p macro.
-  Less powerful, but more convenient for simple use cases."
-  [vars expr]
-  `(eval
-    `(macros/symbol-macrolet
-      [~@'~(mapcat (fn [x] `(~(first x) ~(second x)))
-                   (group vars 2))]
-      ~~expr)))
+(defn with-all-p* [box & body]
+  `(p-eval ~box
+           `(with-all-p ~'~box
+              ~@'~body)))
+
+;; so if we want this to work as a function, where
+;; we can pass in the box and the expression, we can
+;; do something like this:
+(defn p-eval*
+  "Evaluate an expression with lexical bindings.
+  Allows for tunneling bindings into an eval expression.
+  "
+  [box expr]
+  (eval `(list (fn? ~box)
+               '~expr)))
